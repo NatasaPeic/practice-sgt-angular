8bc415702ca8f4bd5b58fa9a99831a3b2e785a8e
NatasaPeic <peic.natasa@gmail.com>
2016-11-07 20:36:12 -0500

    Modify insert.php

--- insert.php
+++ insert.php
@@ -1,12 +1,12 @@
 <?php
 $data = json_decode(file_get_contents("php://input"));
 $id = mysql_real_escape_string($data->id);
-$firstName = mysql_real_escape_string($data->firstName);
-$lastName = mysql_real_escape_string($data->lastName);
+$first_name = mysql_real_escape_string($data->first_name);
+$last_name = mysql_real_escape_string($data->last_name);
 $address = mysql_real_escape_string($data->address);
 $city = mysql_real_escape_string($data->city);
 
 mysql_connect("localhost", "root", "");
 mysql_select_db("student_registry");
-mysql_query("INSERT INTO students (`first_name`, `last_name`, `address`, `city`) VALUES('".$id."', '".$firstName."', '".$lastName."', '".$address."', '".$city."')");
+mysql_query("INSERT INTO students (`first_name`, `last_name`, `address`, `city`) VALUES('".$id."', '".$first_name."', '".$last_name."', '".$address."', '".$city."')");
  ?>
